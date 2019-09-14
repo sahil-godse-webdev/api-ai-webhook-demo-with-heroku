4189c273fed9778b59b883c179b94c507baf9f33
GODse Sahil <42857289+sahil-godse-webdev@users.noreply.github.com>
2019-09-14 09:07:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -28,8 +28,8 @@ if($method == 'POST'){
 	}
 
 	$response = new \stdClass();
-	$response->speech = "";
-	$response->displayText = "";
+	$response->speech = $speech;
+	$response->displayText = $$speech;
 	$response->source = "webhook";
 	echo json_encode($response);
 }
